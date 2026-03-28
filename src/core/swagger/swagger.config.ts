809d17759f86d3b547a1e2bc62d6942153fb3a83
flowihs <zmulenko500@gmail.com>
2026-03-28 18:38:41 +0300

    fix(swagger): load ui assets from cdn for vercel support

--- src/core/swagger/swagger.config.ts
+++ src/core/swagger/swagger.config.ts
@@ -19,5 +19,12 @@ export function setupSwagger(app: INestApplication): void {
 		.build();
 
 	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup("swagger", app, document);
+	SwaggerModule.setup("swagger", app, document, {
+		customCssUrl:
+			"https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css",
+		customJs: [
+			"https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.js",
+			"https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.js"
+		]
+	});
 }
