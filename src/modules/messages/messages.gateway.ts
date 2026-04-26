9b47a1e3b6e1b48b78e8ea82b4d5bfb8ab9f8078
flowihs <zmulenko500@gmail.com>
2026-04-26 12:14:29 +0300

    fix railway

---
+++ src/modules/messages/messages.gateway.ts
@@ -0,0 +1,8 @@
+// import { WebSocketGateway } from "@nestjs/websockets";
+//
+// import { MessagesService } from "./messages.service";
+//
+// @WebSocketGateway()
+// export class MessagesGateway {
+// 	constructor(private readonly messagesService: MessagesService) {}
+// }
